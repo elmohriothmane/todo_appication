68e0462f93219db525cf126afb51bc785978f79a
ELMOHRI othmane <o.elmohri@esi-sba.dz>
2022-04-29 15:09:54 +0200

    add react project with TodoListItem component

---
+++ Front/todo-app/src/components/TodoListItem/TodoListItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class TodoListItem extends Component {
+  render() {
+    return (
+      <div>TodoListItem</div>
+    )
+  }
+}
+
+
+export default TodoListItem;
