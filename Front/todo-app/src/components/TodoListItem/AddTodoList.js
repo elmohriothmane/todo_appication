206d43396a5d444f28ed312c7d72e9e4d3806ab4
ELMOHRI othmane <o.elmohri@esi-sba.dz>
2022-04-29 17:05:52 +0200

    add addTodoLIst form

--- Front/todo-app/src/components/TodoListItem/AddTodoList.js
+++ Front/todo-app/src/components/TodoListItem/AddTodoList.js
@@ -1,12 +1,64 @@
 import React, { Component } from 'react'
 
 class AddTodoList extends Component {
+
+  constructor(){
+      super()
+
+      this.state={
+        listName:"",
+        description:""
+      }
+
+      this.onChange=this.onChange.bind(this);
+      this.onSubmit= this.onSubmit.bind(this);
+  }
+
+  onChange(e){
+      this.setState({[e.target.name]:e.target.value})
+  }
+
+  onSubmit(e){
+
+      e.preventDefault()
+      const newTodoList ={
+        listName:this.state.listName,
+        description:this.state.description,
+      }
+
+      console.log(newTodoList);
+  }
   render() {
     return (
-      <div>
+        <div className="register">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8 m-auto">
+                    <h5 className="display-4 text-center">Create / Edit A New List form</h5>
+                    <hr />
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <input type="text" className="form-control form-control-lg " placeholder="Todo List Title" name="listName" 
+                                value={this.state.listName}
+                                onChange={this.onChange}
+                            />
+                        </div>
+                        
+                        <br/>
+                        <div className="form-group">
+                            <textarea className="form-control form-control-lg" placeholder="TodoList Description" name="description"
+                            value={this.state.description}
+                            onChange={this.onChange} ></textarea>
+                        </div>
+                        <br/>
 
-        <h1>Add Todo List Form</h1>
-      </div>
+                        <input type="submit" className="btn btn-primary btn-block mt-4" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
     )
   }
 }
