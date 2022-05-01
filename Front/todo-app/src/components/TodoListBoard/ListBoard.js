b64e3a6b55260ef68fc43f69e702ff895b60bfd6
ELMOHRI othmane <o.elmohri@esi-sba.dz>
2022-05-01 14:18:47 +0200

    edit TodoList Task component

--- Front/todo-app/src/components/TodoListBoard/ListBoard.js
+++ Front/todo-app/src/components/TodoListBoard/ListBoard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link, useParams } from "react-router-dom";
+import TodoListTask from './TodoListTask/TodoListTask';
 
 
 
@@ -22,51 +23,8 @@ function withParams(Component) {
             <i className="fas fa-plus-circle"> Create TODO List Task</i>
         </Link>
         <br />
-        <hr />
-        <div className="container">
-            <div className="row">
-                <div className="col-md-4">
-                    <div className="card text-center mb-2">
-                        <div className="card-header bg-secondary text-white">
-                            <h3>TO DO</h3>
-                        </div>
-                    </div>
-
-                    <div className="card mb-1 bg-light">
-
-                       
-                        <div className="card-body bg-light">
-                            <h5 className="card-title">Todo list title</h5>
-                            <p className="card-text text-truncate ">
-                            Todo list description
-                            </p>
-                            <a  className="btn btn-primary">
-                                View / Update
-                            </a>
-
-                            <button className="btn btn-danger ms-4">
-                                Delete
-                            </button>
-                        </div>
-                    </div>
-
-                </div>
-                <div className="col-md-4">
-                    <div className="card text-center mb-2">
-                        <div className="card-header bg-primary text-white">
-                            <h3>In Progress</h3>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div className="card text-center mb-2">
-                        <div className="card-header bg-success text-white">
-                            <h3>Done</h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <hr />  
+        <TodoListTask/>
 
     </div>
     )
